2dec5949e782b422a6212207a7f816d94e0f3ae4
Silt <sltapia@gmail.com>
2014-07-20 14:05:51 +0200

    Final test

--- proyect/ViewController.m
+++ proyect/ViewController.m
@@ -18,7 +18,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    // sip esto parece que tira
+    //==========================
+    //**************************
+    //==========================
 }
 
 - (void)didReceiveMemoryWarning
